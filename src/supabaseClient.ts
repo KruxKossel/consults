8afcd91a9415ef28edd8b31c8b1bcf35eb4f24cd
Ellen.Kos <143337580+KruxKossel@users.noreply.github.com>
2025-02-22 23:15:50 -0300

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -4,8 +4,11 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Supabase URL e Key são obrigatórias.");
+if (!supabaseUrl) {
+  throw new Error("Supabase URL é obrigatória.");
+}
+if (!supabaseKey) {
+  throw new Error("Supabase Key é obrigatória.");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
